596e2e53effe738dc0f50f607d3b09e7491a3cb1
SukaNahuiBlyat <dvadva92@inbox.ru>
2024-04-10 23:57:05 +0300

    version 2 це пизда

--- src/api/Store.js
+++ src/api/Store.js
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {userapi} from "./userApi";
+import {withdrawsapi} from "./withdrawsApi";
+import {methodsapi} from "./methodsApi";
+import {applicationsapi} from "./applicationsApi";
+import {teamsapi} from "./teamsApi";
+
 
 
 export const store = configureStore({
     reducer: {
         [userapi.reducerPath]: userapi.reducer,
+        [withdrawsapi.reducerPath]: withdrawsapi.reducer,
+        [methodsapi.reducerPath]: methodsapi.reducer,
+        [applicationsapi.reducerPath]: applicationsapi.reducer,
+        [teamsapi.reducerPath]: teamsapi.reducer,
     },
     middleware: (getDefaultMiddlware) => getDefaultMiddlware()
         .concat(userapi.middleware)
