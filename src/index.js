dafcf3eeb8d2cdc432be8cfbcd23255206dcebd2
SukaNahuiBlyat <dvadva92@inbox.ru>
2024-04-11 14:03:56 +0300

    version 2 це пизда

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import {store} from "./api/Store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-      {/*<React.StrictMode>*/}
+      <React.StrictMode>
           <BrowserRouter>
               <App />
           </BrowserRouter>
-      {/*</React.StrictMode>*/}
+      </React.StrictMode>
     </Provider>
 );
 
