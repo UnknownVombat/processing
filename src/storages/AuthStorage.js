8cdeb351ffea66f305622dbad093e972c2526ca3
SukaNahuiBlyat <dvadva92@inbox.ru>
2024-04-11 20:44:07 +0300

    version 2 це пизда

--- src/storages/AuthStorage.js
+++ src/storages/AuthStorage.js
@@ -3,7 +3,7 @@ import {createJSONStorage, persist} from "zustand/middleware";
 
 export const authStorage = create(persist(
     (set)=> ({
-        key: '',
+        key: "",
         resetKey: (new_key) => set(() => ({key: new_key}))
     }),
     {name: 'auth_data_storage',
