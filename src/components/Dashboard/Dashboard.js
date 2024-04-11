09115e6bd12876d63a0d87336d36e76db2df8ead
SukaNahuiBlyat <dvadva92@inbox.ru>
2024-04-11 16:04:01 +0300

    version 2 це пизда

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,7 +2,6 @@
 import React from "react";
 import styles from './Dashboard.module.css'
 import {authStorage} from "../../storages/AuthStorage";
-import {dataStorage} from "../../storages/DataStorage";
 import UserRow from "./UserRow/UserRow";
 import SessionRow from "./SessionRow/SessionRow";
 import {userapi} from "../../api/userApi";
@@ -16,18 +15,15 @@ const Dashboard = () => {
     // const setSessions = dataStorage((state) => state.resetSessions)
     const header = {'Authorization': key}
     const {data: workersData, error: workersError, isError: workersIsError} = userapi.useWorkersQuery(header)
-    // const [status, setStatus] = useState('admin')
-    const status = 'admin'
     const navigate = useNavigate()
-    if (workersData) {
-        alert('Получил данные, не обновляю вообще нихуя')
-        // setStatus(workersData['status'])
-        // if (workersData['status'] === 'admin') {
-        //     setUsers(workersData['users'])
-        //     setSessions(workersData['sessions'])
-        // }
-        // resetStatus(workersData['user']['name'], workersData['user']['balance'], workersData['user']['status'])
-    }
+    // if (workersData) {
+    //     // setStatus(workersData['status'])
+    //     // if (workersData['status'] === 'admin') {
+    //     //     setUsers(workersData['users'])
+    //     //     setSessions(workersData['sessions'])
+    //     // }
+    //     // resetStatus(workersData['user']['name'], workersData['user']['balance'], workersData['user']['status'])
+    // }
     if (workersIsError) {
         if (workersError.status === 401) {
             console.error(workersError)
@@ -39,45 +35,48 @@ const Dashboard = () => {
     // if (workersLoading) {
     //     return <div>Loading</div>
     // }
-    const user = dataStorage((state) => state.user)
-    const users = dataStorage((state) => state.users)
-    const sessions = dataStorage((state) => state.sessions)
-    if (status === 'user'){
-        return (
-            <div className={styles.user_dashboard}>
-                <h2>Дашборд</h2>
-                <div className={styles.user_row}>
-                    <p>Имя: {user['name']}</p>
-                    <p>Баланс: {user['balance']}</p>
-                    <p>Статус: {user['status']}</p>
-                    <WithdrawBlock />
+    // const user = dataStorage((state) => state.user)
+    // const users = dataStorage((state) => state.users)
+    // const sessions = dataStorage((state) => state.sessions)
+    if (workersData) {
+        if (workersData['status'] === 'user'){
+            return (
+                <div className={styles.user_dashboard}>
+                    <h2>Дашборд</h2>
+                    <div className={styles.user_row}>
+                        <p>Имя: {workersData['user']['name']}</p>
+                        <p>Баланс: {workersData['user']['balance']}</p>
+                        <p>Статус: {workersData['user']['status']}</p>
+                        <WithdrawBlock />
+                    </div>
                 </div>
-            </div>
-        );
-    } else {
-        return (
-            <div className={styles.all_dashboard}>
-                <h2>Дашборд</h2>
-                <div className={styles.your_data}>
-                    <p className={styles.big_p}>Ваши данные:</p>
-                    <p className={styles.little_p}>Имя: {user['name']} Баланс: {user['balance']} Статус: {user['status']}</p>
-                </div>
-                <div className={styles.dashboard}>
-                    <div className={styles.user_dashboard}>
-                        <div className={styles.users_row}>
-                            <p className={styles.big_p}>Данные о команде:</p>
-                            {users.map((element) => {return UserRow(element)})}
-                            <WithdrawBlock />
+            );
+        } else {
+            return (
+                <div className={styles.all_dashboard}>
+                    <h2>Дашборд</h2>
+                    <div className={styles.your_data}>
+                        <p className={styles.big_p}>Ваши данные:</p>
+                        <p className={styles.little_p}>Имя: {workersData['user']['name']} Баланс: {workersData['user']['balance']} Статус: {workersData['user']['status']}</p>
+                    </div>
+                    <div className={styles.dashboard}>
+                        <div className={styles.user_dashboard}>
+                            <div className={styles.users_row}>
+                                <p className={styles.big_p}>Данные о команде:</p>
+                                {workersData['users'].map((element) => {return UserRow(element)})}
+                                <WithdrawBlock />
+                            </div>
+                        </div>
+                        <div className={styles.user_dashboard}>
+                            <p className={styles.session_p}>Данные о сессиях:</p>
+                            {workersData['sessions'].map((element) => {return SessionRow(element['WorkersSessions'], key)})}
                         </div>
                     </div>
-                    <div className={styles.user_dashboard}>
-                        <p className={styles.session_p}>Данные о сессиях:</p>
-                        {sessions.map((element) => {return SessionRow(element['WorkersSessions'], key)})}
-                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 };
+
 
 export default Dashboard;
