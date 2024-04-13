c7966b7d479016a872d97a4d6654e0f00668049d
SukaNahuiBlyat <dvadva92@inbox.ru>
2024-04-13 23:02:14 +0300

    version 3 итоговая?

--- src/components/Dashboard/WithdrawBlock/WithdrawBlock.js
+++ src/components/Dashboard/WithdrawBlock/WithdrawBlock.js
@@ -42,7 +42,7 @@ const WithdrawBlock = () => {
             <label>Вывести баланс</label>
             <input id='amount' type='number' placeholder='Сумма вывода' min={0}/>
             <button className={styles.submit} onClick={withdraw}>Вывести</button>
-            <p>Код гарантекс для последней заявки на вывод: {codeData['access'] === false ? '': codeData['result']}</p>
+            <p>Код гарантекс для последней заявки на вывод: {codeData ? codeData['access'] === false ? '': codeData['result']: null}</p>
         </div>
     );
 };
